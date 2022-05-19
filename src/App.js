188c83f9632c298a34ae0a7157a21f8b190d6daf
PrasannaKumar999 <97685872+PrasannaKumar999@users.noreply.github.com>
2022-05-19 22:30:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Function from "./function";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Function />
+      </div>
+    );
+  }
+}
+export default App;
